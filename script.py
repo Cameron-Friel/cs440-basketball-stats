dbd6ffb1b024d06e67dce665efd246c38f274315
andrekyl <andrekyl@oregonstate.edu>
2019-03-12 19:38:04 -0700

    Script creates a table now!!!

--- script.py
+++ script.py
@@ -1,28 +1,30 @@
 import pymysql.cursors
+import json
+import time
+
+fantasyFileRef = './PlayerStats/PlayerStats-15-16.json'
+with open(fantasyFileRef) as f:
+    fantasyData = json.load(f)
 
 # Connect to the database
 connection = pymysql.connect(host='classmysql.engr.oregonstate.edu',
-                             user='user', #cs440_onid
-                             password='password', #password
-                             db='db', #database name
+                             user='cs440_andrekyl', #cs440_onid
+                             password='0005', #password
+                             db='cs440_andrekyl', #database name
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-try:
-    with connection.cursor() as cursor:
-        # Create a new record
-        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
 
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
+try:
+    for player in fantasyData:
+        print(player['id'])
+        with connection.cursor() as cursor:
+            # Change the table name for the insert
+            if player.get('salary'):
+                sql = "INSERT INTO `players-15-16` (`id`, `full_name`, `points`, `assists`, `rebounds`, `steals`, `salary`, `fantasy_points`, `blocks`) VALUES (%s, %s, %s, %s ,%s, %s, %s, %s, %s)"
+                cursor.execute(sql, (str(player['id']), str(player['full_name']), str(player['points']), str(player['assists']), str(player['rebounds']), str(player['steals']), str(player['salary']), str(player['fantasy_points']), str(player['blocks']))) #populate the data
+    
     connection.commit()
 
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-        cursor.execute(sql, ('webmaster@python.org',))
-        result = cursor.fetchone()
-        print(result)
 finally:
     connection.close()
